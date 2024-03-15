762dcec907046967b1367fac31b115cac7ca3913
Bladers123 <tristangehring123@hotmail.de>
2024-03-15 19:10:44 +0100

    create Level

---
+++ levels/level1.js
@@ -0,0 +1,29 @@
+const level1 = new Level(
+    [
+        new Enemy(),
+        new Enemy(),
+        new Enemy()
+    ],
+    [
+        new BackgroundOject('../img/3. Background/Layers/5. Water/D1.png', 0),
+        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D1.png', 0),
+        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D1.png', 0),
+        new BackgroundOject('../img/3. Background/Layers/2. Floor/D1.png', 0),
+        new BackgroundOject('../img/3. Background/Layers/1. Light/1.png', 0),
+        new BackgroundOject('../img/3. Background/Layers/5. Water/D2.png', 720),
+        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D2.png', 720),
+        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D2.png', 720),
+        new BackgroundOject('../img/3. Background/Layers/2. Floor/D2.png', 720),
+        new BackgroundOject('../img/3. Background/Layers/1. Light/2.png', 720),
+        new BackgroundOject('../img/3. Background/Layers/5. Water/D1.png', 720*2),
+        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D1.png', 720*2),
+        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D1.png', 720*2),
+        new BackgroundOject('../img/3. Background/Layers/2. Floor/D1.png', 720*2),
+        new BackgroundOject('../img/3. Background/Layers/1. Light/1.png', 720*2),
+        new BackgroundOject('../img/3. Background/Layers/5. Water/D2.png', 720*3),
+        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D2.png', 720*3),
+        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D2.png', 720*3),
+        new BackgroundOject('../img/3. Background/Layers/2. Floor/D2.png', 720*3),
+        new BackgroundOject('../img/3. Background/Layers/1. Light/2.png', 720*3),
+    ]
+);
--- models/world.class.js
+++ models/world.class.js
@@ -5,34 +5,8 @@ class World {
     cameraX = 0;
     character;
 
-    enemies = [
-        new Enemy(),
-        new Enemy(),
-        new Enemy()
-    ];
-
-    backgroundObjects = [
-        new BackgroundOject('../img/3. Background/Layers/5. Water/D1.png', 0),
-        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D1.png', 0),
-        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D1.png', 0),
-        new BackgroundOject('../img/3. Background/Layers/2. Floor/D1.png', 0),
-        new BackgroundOject('../img/3. Background/Layers/1. Light/1.png', 0),
-        new BackgroundOject('../img/3. Background/Layers/5. Water/D2.png', 720),
-        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D2.png', 720),
-        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D2.png', 720),
-        new BackgroundOject('../img/3. Background/Layers/2. Floor/D2.png', 720),
-        new BackgroundOject('../img/3. Background/Layers/1. Light/2.png', 720),
-        new BackgroundOject('../img/3. Background/Layers/5. Water/D1.png', 720*2),
-        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D1.png', 720*2),
-        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D1.png', 720*2),
-        new BackgroundOject('../img/3. Background/Layers/2. Floor/D1.png', 720*2),
-        new BackgroundOject('../img/3. Background/Layers/1. Light/1.png', 720*2),
-        new BackgroundOject('../img/3. Background/Layers/5. Water/D2.png', 720*3),
-        new BackgroundOject('../img/3. Background/Layers/4.Fondo 2/D2.png', 720*3),
-        new BackgroundOject('../img/3. Background/Layers/3.Fondo 1/D2.png', 720*3),
-        new BackgroundOject('../img/3. Background/Layers/2. Floor/D2.png', 720*3),
-        new BackgroundOject('../img/3. Background/Layers/1. Light/2.png', 720*3),
-    ];
+    enemies = level1.enemies;
+    backgroundObjects = level1.backgroundObjects;
 
     constructor(canvas, character) {
         this.context = canvas.getContext('2d');
