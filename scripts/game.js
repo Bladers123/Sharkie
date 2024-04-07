3452b5dcc0e519f49f6730240fe16ed4e06eb403
Bladers123 <tristangehring123@hotmail.de>
2024-04-07 16:07:45 +0200

    responsive

--- scripts/game.js
+++ scripts/game.js
@@ -245,15 +245,15 @@ function getIntroductionsTemplate() {
     <div class="introductions-content">
         <div class="move">
             <img class="introductions-move-with-arrows-img" src="img/6.Botones/Key/arrow keys.png" alt="arrow-keys">
-            <img src="img/6.Botones/Tittles/Move title.png" alt="move">
+            <img class="arrows-text" src="img/6.Botones/Tittles/Move title.png" alt="move">
         </div>
         <div class="attack">
             <img class="introductions-attack-with-d-img" src="img/6.Botones/Key/D key.png" alt="d-key">
-            <img src="img/6.Botones/Tittles/Attack tittle.png" alt="attack">
+            <img class="attack-with-d-text" src="img/6.Botones/Tittles/Attack tittle.png" alt="attack">
         </div>
         <div class="attack">
             <img class="introductions-attack-with-space-img" src="img/6.Botones/Key/Space Bar key.png" alt="space-key">
-            <img src="img/6.Botones/Tittles/Attack tittle.png" alt="attack">
+            <img class="attack-with-space-text" src="img/6.Botones/Tittles/Attack tittle.png" alt="attack">
         </div>
     </div>
     `;
