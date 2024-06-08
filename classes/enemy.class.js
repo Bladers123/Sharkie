72f2d8e7c9597fb489e11040e9a34c7951667e27
Bladers123 <tristangehring123@hotmail.de>
2024-06-08 02:11:06 +0200

    test

--- classes/enemy.class.js
+++ classes/enemy.class.js
@@ -4,11 +4,11 @@
  */
 class Enemy extends MovableObject {
     imagesOfNormalPuffer = [
-        '../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png',
-        '../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png',
-        '../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png',
-        '../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png',
-        '../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png'
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png'
     ];
 
     imagesOfTransitionPuffer = [
